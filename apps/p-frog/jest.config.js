4dfda9ce6a4d53f9dc9e0537cc01e0541107c78c
Yotam Achrak <yotam2585@gmail.com>
2022-01-06 12:15:12 +0200

    initial commit

---
+++ apps/p-frog/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'p-frog',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/p-frog',
+};
